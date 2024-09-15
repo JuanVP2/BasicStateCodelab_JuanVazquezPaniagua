ed04d828220d12eb42a1b56c5b9cc357e28de2ee
Juan <Juan@DESKTOP-JVP>
2024-09-14 18:22:58 -0600

    Estado en Jetpack Compose

--- app/src/main/java/com/example/basicstatecodelab/ui/theme/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/ui/theme/WaterCounter.kt
@@ -12,14 +12,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Row
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-
-
-        Text("You've had $count glasses.")
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
         Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
